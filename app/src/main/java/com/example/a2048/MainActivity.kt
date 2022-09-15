35cc8fb917086ac43f77cfb9e3eabd7fc198ec25
Kofeins <Korotkov.NM@yandex.ru>
2022-09-15 10:01:16 +0300

    2048 new Algorithm(L,R -- good. T, B -- bad) smth wrong with rages

--- app/src/main/java/com/example/a2048/MainActivity.kt
+++ app/src/main/java/com/example/a2048/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.a2048
 import android.accessibilityservice.GestureDescription
 import android.content.Context
 import android.gesture.Gesture
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.TextView
@@ -38,6 +40,8 @@ class MainActivity : AppCompatActivity() {
                 S!![i][j] = findViewById(findId)
             }
         }
+
+
 
         //R shift(0, 0, 4, 3, 0, 1)
         //L shift(0, 3, 4, 0, 0, -1)
@@ -74,11 +78,11 @@ class MainActivity : AppCompatActivity() {
                 if(Math.abs(diffx) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOSITY_THRESHOLD){
                     if (diffx > 0){
                         //right swipe
-                        this@MainActivity.OnSwipeRigth()
+                        this@MainActivity.onSwipeRigth()
                     }
                     else{
                         //left swipe
-                        this@MainActivity.OnSwipeLeft()
+                        this@MainActivity.onSwipeLeft()
                     }
                     true
                 } else {
@@ -89,12 +93,12 @@ class MainActivity : AppCompatActivity() {
                 if(Math.abs(diffy) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOSITY_THRESHOLD){
                     if (diffx > 0){
                         //right swipe
-                        this@MainActivity.OnSwipeBottom()
+                        this@MainActivity.onSwipeBottom()
 
                     }
                     else{
                         //left swipe
-                        this@MainActivity.OnSwipeTop()
+                        this@MainActivity.onSwipeTop()
                     }
                     true
                 } else{
@@ -108,48 +112,133 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-// АЛГОРИТМ -- работает неправильно(сори, забыл стереть импульсивный комент Х) ). ПЕРЕДЕЛАТЬ!!!!!!! НУ и да. Пресчитать цифры!))))
-    private fun shift(p1: Int, p2 :Int, p3 :Int, p4 :Int, s1: Int, s2: Int){
-        val m1: Int = p3
-        val m2: Int = p4
-        var i: Int = p1
-        var j: Int = p2
-        val si: Int = s1 // изменение i
-        val sj: Int = s2 // изменение j
-        while (i != m1){
-            j = p2
-            while (j != m2){
-                if (nums[i][j] == nums[i + si][j + sj]){
-                    nums[i + si][j + sj] = nums[i][j] * 2
-                    nums[i][j] = 0
+    private fun newShift(param: Char){
+        var range1: IntProgression? = null
+        var range2: IntProgression? = null
+
+        var add: Int = 1
+
+
+        when(param){
+            ('L') -> {//i j
+                range1 = (0..3)
+                range2 = (0..2)
+                add = 1
+            }
+            'R' -> {//i j
+                range1 = (3 downTo 0)
+                range2 = (3 downTo 1)
+                add = -1
+
+            }
+            'T' -> {//j i
+                range1 = (0..3)
+                range2 = (0..2)
+                add = 1
+            }
+            'B' -> {//j i
+                range1 = (3 downTo 0)
+                range2 = (3 downTo 1)
+                add = -1
+
+            }
+
+        }
+
+        findPair(range1!!, range2!!, param, add)
+        Log.d("MyLog", "ERROR HERE last work")
+        turnTo(range1!!, param, add)
+
+        randomS()
+        rename()
+
+    }
+// РАБОТАЕТ ИДЕАЛЬНО
+    private fun findPair(range1: IntProgression, range2: IntProgression, param: Char, add: Int) {
+        //L 0 2 3
+
+        var i: Int = 0 //stroke
+        var j: Int = 0 // colomns
+        var next: Int = 0
+        for (s in range1){
+
+            for (c in range2){
+
+                if (param == 'L' || param == 'R'){
+                    i = s
+                    j = c
+
+                    next = nums[i][j + add]
+                    if (nums[i][j] == next && nums[i][j] != 0){
+                        nums[i][j] *= 2
+                        nums[i][j + add] = 0
+                    }
                 }
-                else if(nums[i + si][j + sj] == 0){
-                    nums[i + si][j + sj] = nums[i][j]
-                    nums[i][j] = 0
+                else if (param == 'T' || param == 'B'){
+                    i = c
+                    j = s
+                    next = nums[i + add][j]
+                    if (nums[i][j] == next && nums[i][j] != 0){
+                        nums[i][j] *= 2
+                        nums[i + add][j] = 0
+                    }
                 }
-                if (sj != 0){
-                    j += sj
+            }
+        }
+
+    }
+/// ТУТ КАКАЯ ТО ОБИШКА
+    private fun turnTo(range1: IntProgression, param: Char, add: Int){
+        var i: Int = 0 //stroke
+        var j: Int = 0 // colomns
+    var zero: Int = -1
+        for (s in range1){
+            zero = -1
+            for (c in range1){
+                if (param == 'L' || param == 'R'){
+
+                    i = s
+                    j = c
+                    if (nums[i][j] == 0 && zero == -1){
+                        zero = j
+                    }
+                    else if(nums[i][j] != 0 && zero != j && zero != -1){
+                        Log.d("MyLog", "nums[$i][$j] = ${nums[i][j]} -----> zero = $zero, PRE")
+                        nums[i][zero] = nums[i][j]
+                        nums[i][j] = 0
+                        zero += add
+                        Log.d("MyLog", "nums[$i][$j] = ${nums[i][j]}, zero = $zero, param = $param")
+
+                    }
+
                 }
-                else{
-                    j++
+                else if (param == 'T' || param == 'B'){
+                    i = c
+                    j = s
+                    if (nums[i][j] == 0 && zero == -1){
+                        zero = i
+                    }
+                    else if(nums[i][j] != 0 && zero != j && zero != -1){
+                        nums[zero][j] = nums[i][j]
+                        nums[i][j] = 0
+                        zero += add
+                    }
                 }
 
             }
-            if (si != 0){
-                i += si
-            }
-            else{
-                i++
-            }
         }
-        randomS()
-        rename()
     }
+
 
     private fun rename(){
         for(i in 0..3){
             for (j in 0..3) {
                 S!![i][j].text = nums[i][j].toString()
+                //if(S!![i][j].text == "0"){
+                   // S!![i][j].setBackgroundColor(Color.parseColor("##FFD700"))
+
+
+                //}
             }
         }
     }
@@ -167,29 +256,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //R shift(0, 0, 4, 3, 0, 1)
-    //L shift(0, 3, 4, 0, 0, -1)
-    //Down shift(0, 0, 3, 4, 1, 0)
-    //Up shift(3, 0, 0, 4, -1, 0)
+    private fun onSwipeBottom() {
 
-    private fun OnSwipeBottom() {
+        //shift(0, 0, 3, 4, 1, 0)
+        newShift('B')
         Toast.makeText(this, "BottomSwipe", Toast.LENGTH_LONG).show()
-        shift(0, 0, 3, 4, 1, 0)
     }
 
-    private fun OnSwipeTop() {
+    private fun onSwipeTop() {
+
+        //shift(3, 0, 0, 4, -1, 0)
+        newShift('T')
         Toast.makeText(this, "TopSwipe", Toast.LENGTH_LONG).show()
-        shift(3, 0, 0, 4, -1, 0)
     }
 
-    private fun OnSwipeLeft() {
+    private fun onSwipeLeft() {
+
+        //shift(0, 3, 4, 0, 0, -1)
+        newShift('L')
         Toast.makeText(this, "LeftSwipe", Toast.LENGTH_LONG).show()
-        shift(0, 3, 4, 0, 0, -1)
     }
 
-    private fun OnSwipeRigth() {
+    private fun onSwipeRigth() {
+
+        //shift(0, 0, 4, 3, 0, 1)
+        newShift('R')
         Toast.makeText(this, "RightSwipe", Toast.LENGTH_LONG).show()
-        shift(0, 0, 4, 3, 0, 1)
 
     }
 
